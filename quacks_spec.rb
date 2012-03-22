116344fbf79cc4eb3fcf44f1b40cea143dc14619
Jürgen Bickert <juergenbickert@gmail.com>
2012-03-22 22:49:17 +0100

    update spec to represent use cases

--- quacks_spec.rb
+++ quacks_spec.rb
@@ -40,15 +40,6 @@ describe Quacks do
     subject.quotes_for_author(name, db).first.must_equal quote
   end
 
-  it "should retrieve all previously saved quotes" do
-    quote2 = "quote2"
-    subject.save_quote(name, quote, db)
-    subject.save_quote(name, quote2, db)
-    quotes = subject.quotes_for_author(name, db)
-    quotes.first.must_equal quote
-    quotes[1].must_equal quote2
-  end
-
   it "should handle different quotes for different authors" do
     name2 = "author2"
     quote2 = "quote2"
@@ -86,32 +77,10 @@ describe Quacks do
     subject.all_quotes(db).length.must_equal 1
   end
 
-  it "should retrieve all quotes" do
-    name1 = "n1"
-    quote1   = "q1"
-    name2 = "n2"
-    quote2   = "q2"
-    name3 = "n3"
-    quote3   = "q3"
-    subject.save_quote(name1, quote1, db)
-    subject.save_quote(name2, quote2, db)
-    subject.save_quote(name3, quote3, db)
-    subject.all_quotes(db).length.must_equal 3
-    subject.all_quotes(db).first.must_match /^q[123]/
-  end
-
   it "should store an image file path for an author" do
     image_path = "path"
     subject.save_image(name, image_path, db)
     subject.image_path_of_author(name, db).must_equal image_path
   end
 
-  it "change an image file path for an author" do
-    old_image_path = "old_path"
-    image_path = "path"
-    subject.save_image(name, old_image_path, db)
-    subject.image_path_of_author(name, db).must_equal old_image_path
-    subject.save_image(name, image_path, db)
-    subject.image_path_of_author(name, db).must_equal image_path
-  end
 end
