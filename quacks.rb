4854e74a2db72cdbd5fe76e9417b7425a2ee4564
Jürgen Bickert <juergenbickert@gmail.com>
2012-03-22 21:11:15 +0100

    inject database

--- quacks.rb
+++ quacks.rb
@@ -1,39 +1,23 @@
 module Quacks
-  @quotes = Hash.new []
-  @images = {}
-
-  def self.reset
-    reset_quotes
-    reset_images
+  def self.quotes_for_author name, db
+    db.quotes.fetch(name) { [] }
   end
 
-  def self.reset_quotes
-    @quotes = Hash.new { |h, k| h[k] = [] }
+  def self.save_quote name, quote, db
+    (db.quotes[name] << quote).uniq!
   end
 
-  def self.reset_images
-    @images = {}
-  end
-
-  def self.quotes_for_author name
-    @quotes.fetch(name) { [] }
-  end
-
-  def self.save_quote name, quote
-    (@quotes[name] << quote).uniq!
-  end
-
-  def self.all_quotes
-    @quotes.inject [] do |acc, (name, quotes)|
+  def self.all_quotes db
+    db.quotes.inject [] do |acc, (name, quotes)|
       acc.concat quotes
     end
   end
 
-  def self.save_image name, image_path
-    @images[name] = image_path
+  def self.save_image name, image_path, db
+    db.images[name] = image_path
   end
 
-  def self.image_path_of_author name
-    @images.fetch(name)
+  def self.image_path_of_author name, db
+    db.images.fetch(name)
   end
 end
--- quacks_spec.rb
+++ quacks_spec.rb
@@ -1,10 +1,20 @@
 require "minitest/autorun"
 require_relative "quacks"
 
+class InMemoryDB
+  attr_reader :quotes, :images
+
+  def initialize
+    @quotes = Hash.new {|h, k| h[k] = [] }
+    @images = {}
+  end
+end
+
 describe Quacks do
-  attr_reader :name, :quote
+  attr_reader :name, :quote, :db
+
   before(:each) do
-    Quacks.reset
+    @db = InMemoryDB.new
   end
 
   subject do
@@ -17,24 +27,24 @@ describe Quacks do
   end
 
   it "should get no quotes for an unknown author" do
-    subject.quotes_for_author("beck").must_equal []
+    subject.quotes_for_author("beck", db).must_equal []
   end
 
   it "should get one quote for an author" do
-    subject.save_quote(name, quote)
-    subject.quotes_for_author(name).length.must_equal 1
+    subject.save_quote(name, quote, db)
+    subject.quotes_for_author(name, db).length.must_equal 1
   end
 
   it "should retrieve a previously saved quote" do
-    subject.save_quote(name, quote)
-    subject.quotes_for_author(name).first.must_equal quote
+    subject.save_quote(name, quote, db)
+    subject.quotes_for_author(name, db).first.must_equal quote
   end
 
   it "should retrieve all previously saved quotes" do
     quote2 = "quote2"
-    subject.save_quote(name, quote)
-    subject.save_quote(name, quote2)
-    quotes = subject.quotes_for_author(name)
+    subject.save_quote(name, quote, db)
+    subject.save_quote(name, quote2, db)
+    quotes = subject.quotes_for_author(name, db)
     quotes.first.must_equal quote
     quotes[1].must_equal quote2
   end
@@ -42,38 +52,38 @@ describe Quacks do
   it "should handle different quotes for different authors" do
     name2 = "author2"
     quote2 = "quote2"
-    subject.save_quote(name, quote)
-    subject.save_quote(name2, quote2)
-    subject.quotes_for_author(name).first.must_equal quote
-    subject.quotes_for_author(name2).first.must_equal quote2
+    subject.save_quote(name, quote, db)
+    subject.save_quote(name2, quote2, db)
+    subject.quotes_for_author(name, db).first.must_equal quote
+    subject.quotes_for_author(name2, db).first.must_equal quote2
   end
 
   it "should get all quotes for an author" do
     quote1 = "quote1"
     quote2 = "quote2"
     quote3 = "quote3"
-    subject.save_quote(name, quote1)
-    subject.save_quote(name, quote2)
-    subject.save_quote(name, quote3)
-    subject.quotes_for_author(name).length.must_equal 3
+    subject.save_quote(name, quote1, db)
+    subject.save_quote(name, quote2, db)
+    subject.save_quote(name, quote3, db)
+    subject.quotes_for_author(name, db).length.must_equal 3
   end
 
   it "should only get quotes for a specific author" do
     unknown = "unknown"
-    subject.save_quote(name, quote)
-    subject.save_quote(unknown, unknown)
-    subject.quotes_for_author(name).length.must_equal 1
+    subject.save_quote(name, quote, db)
+    subject.save_quote(unknown, unknown, db)
+    subject.quotes_for_author(name, db).length.must_equal 1
   end
 
   it "should ignore duplicate quotes" do
-    subject.save_quote(name, quote)
-    subject.save_quote(name, quote)
-    subject.quotes_for_author(name).length.must_equal 1
+    subject.save_quote(name, quote, db)
+    subject.save_quote(name, quote, db)
+    subject.quotes_for_author(name, db).length.must_equal 1
   end
 
   it "should store quotes" do
-    subject.save_quote(name, quote)
-    subject.all_quotes.length.must_equal 1
+    subject.save_quote(name, quote, db)
+    subject.all_quotes(db).length.must_equal 1
   end
 
   it "should retrieve all quotes" do
@@ -83,25 +93,25 @@ describe Quacks do
     quote2   = "q2"
     name3 = "n3"
     quote3   = "q3"
-    subject.save_quote(name1, quote1)
-    subject.save_quote(name2, quote2)
-    subject.save_quote(name3, quote3)
-    subject.all_quotes.length.must_equal 3
-    subject.all_quotes.first.must_match /^q[123]/
+    subject.save_quote(name1, quote1, db)
+    subject.save_quote(name2, quote2, db)
+    subject.save_quote(name3, quote3, db)
+    subject.all_quotes(db).length.must_equal 3
+    subject.all_quotes(db).first.must_match /^q[123]/
   end
 
   it "should store an image file path for an author" do
     image_path = "path"
-    subject.save_image(name, image_path)
-    subject.image_path_of_author(name).must_equal image_path
+    subject.save_image(name, image_path, db)
+    subject.image_path_of_author(name, db).must_equal image_path
   end
 
   it "change an image file path for an author" do
     old_image_path = "old_path"
     image_path = "path"
-    subject.save_image(name, old_image_path)
-    subject.image_path_of_author(name).must_equal old_image_path
-    subject.save_image(name, image_path)
-    subject.image_path_of_author(name).must_equal image_path
+    subject.save_image(name, old_image_path, db)
+    subject.image_path_of_author(name, db).must_equal old_image_path
+    subject.save_image(name, image_path, db)
+    subject.image_path_of_author(name, db).must_equal image_path
   end
 end
