41361030cf6f7d9cc1aaf47b22be9d273c5e92a6
Jürgen Bickert <juergenbickert@gmail.com>
2012-03-24 07:33:58 +0100

    add author presenter

---
+++ presenter/author_presenter.rb
@@ -0,0 +1,37 @@
+require_relative "../core/get_quotes"
+
+class AuthorPresenter
+  def initialize(author_id, view, db)
+    @author_id = author_id
+    @view = view
+    @db = db
+  end
+
+  def update_view
+    author = @db.get_author(@author_id)
+    quotes = author.quotes
+    @view.author = extract_model_author(author)
+    @view.quotes = extract_model_quotes(quotes)
+  end
+
+  def extract_model_author(author)
+    author_model = {}
+    author_model[:name] = author.name
+    author_model[:id] = author.id
+    author_model[:img_src] = author.image.src if author.image
+    author_model
+  end
+
+  def extract_model_quotes(quotes)
+    quotes_model = []
+    quotes.each do |quote|
+      quote_model = {}
+      quote_model[:id] = quote.id
+      quote_model[:text] = quote.text
+      quote_model[:likes] = quote.likes.count
+      quote_model[:dislikes] = quote.dislikes.count
+      quotes_model << quote_model
+    end
+    quotes_model
+  end
+end
---
+++ spec/presenter/author_presenter_spec.rb
@@ -0,0 +1,97 @@
+require "minitest/autorun"
+require_relative "../../presenter/author_presenter"
+require_relative "../core/memory_db"
+require_relative "../../core/add_author"
+require_relative "../../core/add_user"
+require_relative "../../core/add_image"
+require_relative "../../core/add_quote"
+require_relative "../../core/user_likes_quote"
+require_relative "../../core/user_dislikes_quote"
+
+class MockAuthorView
+  attr_accessor :author, :quotes
+  def initialize
+    @author = {}
+  end
+end
+
+describe AuthorPresenter do
+  attr_reader :author_id, :user_id, :db, :view, :presenter
+
+  before(:each) do
+    @db = InMemoryDB.new
+    t = AddAuthor.new("Uncle Bob", db)
+    t.execute
+    @author_id = t.author_id
+
+    au = AddUser.new("Bill", "billy@example.com", db)
+    au.execute
+    @user_id = au.user_id
+
+    @view = MockAuthorView.new
+    @presenter = AuthorPresenter.new(author_id, view, db)
+  end
+
+  it "should pass name and id to view" do
+    presenter.update_view
+    view.quotes.must_equal []
+    view.author[:name].must_equal "Uncle Bob"
+    view.author[:id].must_equal author_id
+  end
+
+  it "should not have an image src" do
+    presenter.update_view
+    view.author[:img_src].must_be_nil
+  end
+
+  it "should have an image src" do
+    t = AddImage.new(author_id, "src", db)
+    t.execute
+    presenter.update_view
+    view.author[:img_src].must_equal "src"
+  end
+
+  it "should pass quotes to view" do
+    text = "Have a nice day!"
+    aq = AddQuote.new(user_id, author_id, text, db)
+    aq.execute
+    quote_id = aq.quote_id
+    aq = AddQuote.new(user_id, author_id, text + "1", db)
+    aq.execute
+    quote2_id = aq.quote_id
+    presenter.update_view
+    view.quotes.first[:text].must_equal text
+    view.quotes.first[:likes].must_equal 0
+    view.quotes.first[:dislikes].must_equal 0
+    view.quotes.first[:id].must_equal quote_id
+    view.quotes[1][:id].must_equal quote2_id
+  end
+
+  it "should count likes for quotes" do
+    text = "Have a nice day!"
+    aq = AddQuote.new(user_id, author_id, text, db)
+    aq.execute
+    quote_id = aq.quote_id
+    ulq = UserLikesQuote.new(user_id, quote_id, db)
+    ulq.execute
+
+    presenter.update_view
+
+    view.quotes.first[:likes].must_equal 1
+    view.quotes.first[:dislikes].must_equal 0
+  end
+
+  it "should count dislikes for quotes" do
+    text = "Have a nice day!"
+    aq = AddQuote.new(user_id, author_id, text, db)
+    aq.execute
+    quote_id = aq.quote_id
+    ulq = UserDislikesQuote.new(user_id, quote_id, db)
+    ulq.execute
+
+    presenter.update_view
+
+    view.quotes.first[:likes].must_equal 0
+    view.quotes.first[:dislikes].must_equal 1
+  end
+end
